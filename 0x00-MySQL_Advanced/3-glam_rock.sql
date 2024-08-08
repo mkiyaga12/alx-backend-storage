5cf0f944b69b1cd62766395cba37910ce58ae77c
mkiyaga12 <kiyagamuzamiru@gmail.com>
2024-08-08 07:49:06 -0700

    glam

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -7,7 +7,7 @@
  */
 -- Retrieve data
 SELECT band_name,
-    (IFNULL(split, 2024) - formed) AS lifespan -- calculate the lifespan
+    (IFNULL(split, 2022) - formed) AS lifespan -- calculate the lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
